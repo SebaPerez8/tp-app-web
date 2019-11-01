8ca051714c4c44d84d6f61ba2257706f03baccf6
unknown <sebaperez32@gmail.com>
2019-11-01 01:58:04 -0300

    plantilla inicial

--- index.php
+++ index.php
@@ -7,6 +7,21 @@
     <title>Document</title>
 </head>
 <body>
+    <h3>Barrios</h3> 
+        <select name="" id="">
     
+    
+    
+        </select> <br> <br>
+    <h3>Tipo</h3> 
+        <input type="radio" name="tipo" checked value="C"> Casa
+        <input type="radio" name="tipo"  value="D"> Departamento
+        <input type="radio" name="tipo" value="L"> Local
+        <br> <br>
+    <h3>Situacion</h3> 
+        <input type="checkbox" name="venta" id="venta" value="V"> Venta
+        <input type="checkbox" name="alquiler" id="alquiler" value="A"> Alquiler 
+        <br> <br>
+        <input type="submit" value="GRABAR">  
 </body>
 </html>
