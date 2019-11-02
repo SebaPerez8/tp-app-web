b3849dd2776a8b65b5573503c01151c15d6d8e4e
unknown <sebaperez32@gmail.com>
2019-11-02 00:40:16 -0300

    conexion a BD y combobox

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
+<?php
+    include("conectar.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Inmobiliaria</title>
 </head>
 <body>
     <h3>Barrios</h3> 
-        <select name="" id="">
-    
+        <select id="idbarrio">
+            
+            <?php
+                $sql = "SELECT * FROM barrios";
+                $result = mysqli_query($conn, $sql);
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<option value=" . $row['barrio'] . ">" . $row['nombre'] . "</option>";
+                }
+                mysqli_close($conn);
+            ?>
     
     
         </select> <br> <br>
@@ -19,9 +31,9 @@
         <input type="radio" name="tipo" value="L"> Local
         <br> <br>
     <h3>Situacion</h3> 
-        <input type="checkbox" name="venta" id="venta" value="V"> Venta
+        <input type="checkbox" name="venta" id="venta" checked value="V"> Venta
         <input type="checkbox" name="alquiler" id="alquiler" value="A"> Alquiler 
         <br> <br>
-        <input type="submit" value="GRABAR">  
+        <input type="submit" value="CONSULTAR">  
 </body>
 </html>
